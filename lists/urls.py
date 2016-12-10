7b0243160023055ac6dea3a83fe8f72fcd13acd5
Yang Liu <thomasliuy@gmail.com>
2016-12-09 21:02:50 -0500

    lists app has its own urls.py now

--- lists/urls.py
+++ lists/urls.py
@@ -14,12 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
-# from django.contrib import admin
-from lists import views as lists_views
-from lists import urls as lists_urls
+from lists import views
 
 urlpatterns = [
-    # url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', lists_views.home_page, name = 'home'),
-    url(r'^lists/', include(lists_urls))
+    url(r'^(\d+)/$', views.view_list, name = 'view_list'),
+    url(r'^new$', views.new_list, name = 'new_list'),
+    url(r'^(\d+)/add_item$', views.add_item, name = 'add_item')
 ]
