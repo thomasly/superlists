bd04ff918d893e3a8f2dcca1e014bf48315d2712
Yang Liu <thomasliuy@gmail.com>
2016-12-11 10:46:13 -0500

    Use Boottrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,17 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import time
-import unittest
+# import unittest
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Chrome()
 		self.browser.implicitly_wait(3)
 
 	def tearDown(self):
+		self.browser.refresh()
 		self.browser.quit()
 
 	def check_for_row_in_list_table(self, row_text):
@@ -92,13 +93,13 @@ class NewVisitorTest(LiveServerTestCase):
 	def test_layout_and_styling(self):
 		# Edith goes to the home page
 		self.browser.get(self.live_server_url)
-		self.browser.set_window_size(1024, 768)
+		self.browser.set_window_size(1920, 1080)
 
 		# She notice the input box is nicely centered
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertAlmostEqual(
 			inputbox.location['x'] + inputbox.size['width'] / 2,
-			512,
+			960,
 			delta=10
 		)
 
@@ -107,12 +108,12 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox.send_keys("testing\n")
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2,
-			512,
+			960,
 			delta=10
 		)
 
 
 
-if __name__ == "__main__":
-	unittest.main(warnings='ignore')
+# if __name__ == "__main__":
+# 	unittest.main(warnings='ignore')
 
